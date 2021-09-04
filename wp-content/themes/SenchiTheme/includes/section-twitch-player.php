803968e18ed962d52de031ad0a8eaae5352f2ae9
Brandon Nichols <brandonaronnichols@gmail.com>
2021-09-04 14:26:41 -0500

    added the ability to display streamers on the live page

--- wp-content/themes/SenchiTheme/includes/section-twitch-player.php
+++ wp-content/themes/SenchiTheme/includes/section-twitch-player.php
@@ -1,5 +1,8 @@
 <section class="section-twitch-player">
     <div class="twitch-player">
         <?php echo do_shortcode('[getTwitchPlayer]');?>
+        <div class="streamer-container">
+
+        </div>
     </div>
 </section>
--- wp-content/themes/SenchiTheme/src/twitch-api.js
+++ wp-content/themes/SenchiTheme/src/twitch-api.js
@@ -1,4 +1,5 @@
-const users = ["sehkuro", "veluxe_gg", "LicheeLi", "Delicatexo", "y4mikun"];
+// const users = ["sehkuro", "veluxe_gg", "LicheeLi", "Delicatexo", "y4mikun"];
+const users = ["AnEternalEnigma", "boxbox", "HasanAbi", "xQcOW", "Lacari"];
 
 let liveStreams = "";
 let streamers = "";
@@ -32,24 +33,63 @@ function getStreamers() {
   });
 }
 
+function displayStreamers(currentLiveData, allStreamsData) {
+  const container = document.querySelector(".streamer-container");
+  const streamCollection = {};
+
+  allStreamsData.forEach((streamer) => {
+    streamCollection[streamer.display_name] = {
+      image: streamer.profile_image_url,
+      category: "Offline",
+      viewers: 0
+    };
+  });
+
+  currentLiveData.forEach((streamer) => {
+    streamCollection[streamer.user_name]["category"] = streamer.game_name;
+    streamCollection[streamer.user_name]["viewers"] = streamer.viewer_count;
+  });
+
+  for (const [key, value] of Object.entries(streamCollection).sort((a, b) =>
+    a[1].viewers > b[1].viewers ? -1 : 1
+  )) {
+    const streamer = document.createElement("div");
+    streamer.className = "streamer";
+
+    const image = document.createElement("img");
+    image.src = `${value.image}`;
+    image.className = "profile-pic";
+
+    const streamerDetails = document.createElement("div");
+    const name = document.createElement("p");
+    name.textContent += key;
+    name.className = "streamer-name";
+    const category = document.createElement("p");
+    category.textContent += value.category;
+    streamerDetails.appendChild(name);
+    streamerDetails.appendChild(category);
+
+    streamer.appendChild(image);
+    streamer.appendChild(streamerDetails);
+
+    if (value.viewers > 0) {
+      const viewerCount = document.createElement("p");
+      viewerCount.textContent += value.viewers;
+      streamer.appendChild(viewerCount);
+    }
+
+    container.appendChild(streamer);
+  }
+
+  console.log("STREAMERS: ", streamCollection);
+}
+
 Promise.all([getLive(), getStreamers()])
   .then((res) => {
-    console.log("RES: ", res);
+    console.log("LIVE: ", res[0].data.data);
+    console.log("ALL: ", res[1].data.data);
+    displayStreamers(res[0].data.data, res[1].data.data);
   })
   .catch((err) => {
     console.log("ERR: ", err);
   });
-
-// axios
-//   .get(`https://api.twitch.tv/helix/streams?${userCollection}`, {
-//     headers: {
-//       Authorization: "Bearer bqc8xdiix7x0lu1fn7z44iu3o6ct53",
-//       "Client-Id": "cm7mz7zd4jy6pfptik8wwjoomwgkv9"
-//     }
-//   })
-//   .then((res) => {
-//     console.log("Res: ", res);
-//   })
-//   .catch((err) => {
-//     console.log("Error: ", err);
-//   });
