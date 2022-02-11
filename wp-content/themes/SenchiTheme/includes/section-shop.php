7daef77572a68ccf7752431d9d2af23284ddd20e
Brandon Nichols <brandonaronnichols@gmail.com>
2022-02-10 22:45:21 -0600

    added links to thredica for preview images on home page, added border above different member types on team page, changed link for shop to go to shopify instead of threadica

--- wp-content/themes/SenchiTheme/includes/section-shop.php
+++ wp-content/themes/SenchiTheme/includes/section-shop.php
@@ -1,17 +1,23 @@
 <?php require 'site-url.php';?>
 <section class="senchi-shop">
     <div class="merch-container">
-        <div class="person-merch-container">
-            <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Bailey-2-edited.jpg"/>
-            <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Dragon-might.png"/>
-        </div>
-        <div class="person-merch-container">
-            <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Noller-2.jpg"/>
-            <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/visions.png"/>
-        </div>
-        <div class="person-merch-container">
-            <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Pixzeles-2-scaled.jpg"/>
-            <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Rise.png"/>
-        </div>
+        <a href="https://threadica.com/products/senchigami-dragon-might?variant=40340575060173" class="person-merch-container" target="_blank">
+            <div>
+                <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Bailey-2-edited.jpg"/>
+                <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Dragon-might.png"/>
+            </div>
+        </a>
+        <a href="https://threadica.com/products/senchigami-visions" class="person-merch-container" target="_blank">
+            <div>
+                <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Noller-2.jpg"/>
+                <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/visions.png"/>
+            </div>
+        </a>
+        <a href="https://threadica.com/products/rise" class="person-merch-container" target="_blank">
+            <div>
+                <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Pixzeles-2-scaled.jpg"/>
+                <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Rise.png"/>
+            </div>
+        </a>
     </div>
 </section>
