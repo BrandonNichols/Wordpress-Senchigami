a60b95a24c16bc68f1ba3617c999c81245f2bf4f
Brandon Nichols <brandonaronnichols@gmail.com>
2022-02-15 20:23:22 -0600

    changed all the preview links from thredica to senchigami, removed metaslider shortcode and replaced with image in homepage, changed css of homepage for the image

--- wp-content/themes/SenchiTheme/includes/section-shop.php
+++ wp-content/themes/SenchiTheme/includes/section-shop.php
@@ -1,19 +1,19 @@
 <?php require 'site-url.php';?>
 <section class="senchi-shop">
     <div class="merch-container">
-        <a href="https://threadica.com/products/senchigami-dragon-might?variant=40340575060173" class="person-merch-container" target="_blank">
+        <a href="https://senchigami.myshopify.com/collections/all/products/power" class="person-merch-container" target="_blank">
             <div>
                 <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Bailey-2-edited.jpg"/>
                 <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Dragon-might.png"/>
             </div>
         </a>
-        <a href="https://threadica.com/products/senchigami-visions" class="person-merch-container" target="_blank">
+        <a href="https://senchigami.myshopify.com/collections/all/products/short-sleeve-unisex-t-shirt" class="person-merch-container" target="_blank">
             <div>
                 <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Noller-2.jpg"/>
                 <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/visions.png"/>
             </div>
         </a>
-        <a href="https://threadica.com/products/rise" class="person-merch-container" target="_blank">
+        <a href="https://senchigami.myshopify.com/collections/all/products/short-sleeve-unisex-t-shirt-2" class="person-merch-container" target="_blank">
             <div>
                 <img class="person-image" src="<?php echo $site?>wp-content/uploads/2021/08/Pixzeles-2-scaled.jpg"/>
                 <img class="merch" src="<?php echo $site?>wp-content/uploads/2021/08/Rise.png"/>
--- wp-content/themes/SenchiTheme/includes/section-slider.php
+++ wp-content/themes/SenchiTheme/includes/section-slider.php
@@ -1,5 +1,7 @@
+<?php require __DIR__ . '/site-url.php';?>
 <section class="section-slider">
     <div class="slider">
-        <?php echo do_shortcode('[metaslider id="77"]');?>
+        <img src="<?php echo $site?>wp-content/uploads/2021/09/Sempire-final-1-700x300.png" />
+        <?php /*echo do_shortcode('[metaslider id="77"]');*/?>
     </div>
 </section>
